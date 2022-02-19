e058f44c38f7bcf16db2150ab4958407471ea41c
refracc <40345422@live.napier.ac.uk>
2022-02-19 14:25:47 +0000

    Tidied up switch statement.

--- src/coursework/SimulatedAnnealing.java
+++ src/coursework/SimulatedAnnealing.java
@@ -10,34 +10,16 @@ public class SimulatedAnnealing extends NeuralNetwork {
 
     @Override
     public double activationFunction(double v) {
-        switch (Parameters.ACTIVATION) {
-            default -> {
-                return (v > 0) ? v : Math.exp(v) - 1;
-            }
-            case HARD_ELISH -> {
-                return (v < 0) ? Math.max(0, Math.min(1, (v + 1) / 2)) * (Math.exp(v) - 1)
-                        : v * Math.max(0, Math.min(1, (v + 1) / 2));
-            }
-            case LEAKY_RELU -> {
-                return (v > 0) ? v : (v / 100);
-            }
-            case RELU -> {
-                return (v > 0) ? v : -1;
-            }
-            case SELU -> {
-                return (v > 0) ? v * 1.0507009
-                        : 1.0507009 * (1.673263 * Math.exp(v)) - 1.673263;
-            }
-            case STEP -> {
-                return (v <= 0) ? -1.0d : 1.0d;
-            }
-            case SWISH -> {
-                return (v * (1 / (1 + Math.exp(-v))));
-            }
-            case TANH -> {
-                return (v < -20.0d) ? -1.0d : (v > 20.0d) ? 1.0d : Math.tanh(v);
-            }
-        }
+        return switch (Parameters.ACTIVATION) {
+            case HARD_ELISH -> (v < 0) ? Math.max(0, Math.min(1, (v + 1) / 2)) * (Math.exp(v) - 1) : v * Math.max(0, Math.min(1, (v + 1) / 2));
+            case LEAKY_RELU -> (v > 0) ? v : (v / 100);
+            case RELU -> (v > 0) ? v : -1;
+            case SELU -> (v > 0) ? v * 1.0507009 : 1.0507009 * (1.673263 * Math.exp(v)) - 1.673263;
+            case STEP -> (v <= 0) ? -1.0d : 1.0d;
+            case SWISH -> (v * (1 / (1 + Math.exp(-v))));
+            case TANH -> (v < -20.0d) ? -1.0d : (v > 20.0d) ? 1.0d : Math.tanh(v);
+            default -> (v > 0) ? v : Math.exp(v) - 1;
+        };
     }
 
     @Override
