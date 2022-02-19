c1f020f99e08e613c40dc9ae1daef0491e27796f
refracc <40345422@live.napier.ac.uk>
2022-02-19 13:23:19 +0000

    Added SimulatedAnnealing.java and added its Override methods.

---
+++ src/coursework/SimulatedAnnealing.java
@@ -0,0 +1,15 @@
+package coursework;
+
+import model.NeuralNetwork;
+
+public class SimulatedAnnealing extends NeuralNetwork {
+    @Override
+    public double activationFunction(double v) {
+        return 0;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
