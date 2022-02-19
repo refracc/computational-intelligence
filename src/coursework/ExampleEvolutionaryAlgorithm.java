2eb16263ee9a4629c33e97a872fc79e5569897c6
refracc <40345422@live.napier.ac.uk>
2022-02-19 15:53:50 +0000

    Added ExampleEvolutionaryAlgorithm.evaluatePopulation function

--- src/coursework/ExampleEvolutionaryAlgorithm.java
+++ src/coursework/ExampleEvolutionaryAlgorithm.java
@@ -1,22 +1,53 @@
 package coursework;
 
+import model.Fitness;
+import model.Individual;
 import model.NeuralNetwork;
 
+import java.util.List;
+
 /**
- * Implements a basic Evolutionary Algorithm to train a Neural Network
- * 
- * You Can Use This Class to implement your EA or implement your own class that extends {@link NeuralNetwork} 
- * 
+ * Implement an Evolutionary Algorithm (extending {@link NeuralNetwork} to solve the Lunar Landers problem.
+ *
+ * @author Stewart A
  */
 public class ExampleEvolutionaryAlgorithm extends NeuralNetwork {
 
+	/**
+	 * The function that will be setting up the Activation Function for the Neural Network
+	 *
+	 * @param v A {@link Double} value that is given by the {@link NeuralNetwork}.
+	 * @return Some value based on the Activation Type set out in the {@link Parameters} class.
+	 */
 	@Override
 	public double activationFunction(double v) {
-		return 0;
+		return switch (Parameters.ACTIVATION) {
+			case HARD_ELISH -> (v < 0) ? Math.max(0, Math.min(1, (v + 1) / 2)) * (Math.exp(v) - 1) : v * Math.max(0, Math.min(1, (v + 1) / 2));
+			case LEAKY_RELU -> (v > 0) ? v : (v / 100);
+			case RELU -> (v > 0) ? v : -1;
+			case SELU -> (v > 0) ? v * 1.0507009 : 1.0507009 * (1.673263 * Math.exp(v)) - 1.673263;
+			case STEP -> (v <= 0) ? -1.0d : 1.0d;
+			case SWISH -> (v * (1 / (1 + Math.exp(-v))));
+			case TANH -> (v < -20.0d) ? -1.0d : (v > 20.0d) ? 1.0d : Math.tanh(v);
+			default -> (v > 0) ? v : (Math.exp(v) - 1) / 10;
+		};
 	}
 
 	@Override
 	public void run() {
 
 	}
+
+	/**
+	 * Evaluate the fitness of the population.
+	 *
+	 * @param individuals The list of individuals (population) to evaluate.
+	 */
+	private void evaluatePopulation(List<Individual> individuals) {
+		for (Individual i : individuals) {
+			i.fitness = Fitness.evaluate(i, this);
+		}
+	}
+
+
 }
--- src/coursework/SimulatedAnnealing.java
+++ src/coursework/SimulatedAnnealing.java
@@ -7,9 +7,19 @@ import model.NeuralNetwork;
 /**
  * This class is an implementation of the Simulated Annealing algorithm that can be used to attempt to solve the problem of landing lunar landers.
  * By default, the {@link SimulatedAnnealing} class extends {@link NeuralNetwork} to confer to the constraints placed upon the {@link Parameters} class' {@code neuralNetworkClass} variable.
+ *
+ * @author Stewart A
+ * @version 1.26
+ * @since 19/02/2022
  */
 public class SimulatedAnnealing extends NeuralNetwork {
 
+    /**
+     * The function that will be setting up the Activation Function for the Neural Network
+     *
+     * @param v A {@link Double} value that is given by the {@link NeuralNetwork}.
+     * @return Some value based on the Activation Type set out in the {@link Parameters} class.
+     */
     @Override
     public double activationFunction(double v) {
         return switch (Parameters.ACTIVATION) {
@@ -64,14 +74,25 @@ public class SimulatedAnnealing extends NeuralNetwork {
             // Replace the best solution that has been found thus far...
             best = (individual.fitness < best.fitness) ? individual.copy() : best;
 
-            // Cool the system down a little...
+            // Cool the algorithm down a little...
             temperature *= (1 - cooling);
             System.out.println(i + "\t" + best);
+
+            // Write the statistics to the screen
             outputStats();
         }
+        // Save the Neural Network weights.
         saveNeuralNetwork();
     }
 
+    /**
+     * The acceptance rate
+     *
+     * @param currentFitness The fitness value of the current individual
+     * @param newFitness     The fitness value of another individual
+     * @param temperature    The temperature in the SimulatedAnnealing algorithm.
+     * @return 1 if accepted.
+     */
     private double acceptance(double currentFitness, double newFitness, double temperature) {
         return (newFitness < currentFitness) ? 1.0d : Math.exp((currentFitness - newFitness) / temperature);
     }
